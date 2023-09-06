930584db219ad0d308a55f5c7daace9820c3c120
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-09-07 01:27:14 +0530

    new arrivals added

---
+++ pages/modules/home/NewArrivals.tsx
@@ -0,0 +1,39 @@
+import ProductCard from "@/pages/components/ProductCard";
+import React from "react";
+
+const NewArrivals = () => {
+  return (
+    <div className="w-full p-10">
+      <h2 className="my-4 text-3xl font-light text-center">New Arrivals</h2>
+      <div className="w-full grid grid-cols-5 gap-4">
+        <ProductCard
+          title="Earring 1"
+          price="500"
+          image="/assets/images/Earrings-1.jpg"
+        />
+        <ProductCard
+          title="Earring 2"
+          price="500"
+          image="/assets/images/Earrings-2.jpg"
+        />
+        <ProductCard
+          title="Earring 3"
+          price="500"
+          image="/assets/images/Earrings-3.jpg"
+        />
+        <ProductCard
+          title="Earring 4"
+          price="500"
+          image="/assets/images/Earrings-4.jpg"
+        />
+        <ProductCard
+          title="Earring 5"
+          price="500"
+          image="/assets/images/Earrings-5.jpg"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default NewArrivals;
--- pages/modules/home/index.tsx
+++ pages/modules/home/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./Banner";
+import NewArrivals from "./NewArrivals";
 
 const HomeModule = () => {
   return (
     <main>
       <Banner />
+      <NewArrivals />
     </main>
   );
 };
