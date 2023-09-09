4c1226a08ad951143fee93293fe64bd4667a7b78
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-09-09 23:06:01 +0530

    link to product page added

--- components/ProductCard.tsx
+++ components/ProductCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import React, { FC } from "react";
 
 interface ProductCardProps {
@@ -9,21 +10,23 @@ interface ProductCardProps {
 
 const ProductCard: FC<ProductCardProps> = ({ title, price, image }) => {
   return (
-    <div className="w-full aspect-square cursor-pointer hover:scale-105 transition-transform">
-      <div className="w-full h-full relative rounded">
-        <Image
-          src={image}
-          alt="title"
-          className="rounded"
-          fill
-          objectFit="cover"
-        />
+    <Link href={"/product/123"}>
+      <div className="w-full aspect-square cursor-pointer hover:scale-105 transition-transform">
+        <div className="w-full h-full relative rounded">
+          <Image
+            src={image}
+            alt="title"
+            className="rounded"
+            fill
+            objectFit="cover"
+          />
+        </div>
+        <div className="w-full text-center mt-2">
+          <h3 className="text-xl font-semibold">{title}</h3>
+          <h4 className="text-lg">Rs. {price}</h4>
+        </div>
       </div>
-      <div className="w-full text-center mt-2">
-        <h3 className="text-xl font-semibold">{title}</h3>
-        <h4 className="text-lg">Rs. {price}</h4>
-      </div>
-    </div>
+    </Link>
   );
 };
 
